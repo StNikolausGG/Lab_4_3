757554178d5537143b22ef8c51a77e3f986393e6
Nikolay Matskevich <kolyan_krytoj@mail.ru>
2026-02-24 01:04:30 +0700

    Added W1, W2, cycles for counting symbols in each string

--- main.c
+++ main.c
@@ -1,7 +1,28 @@
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
-    printf("Hello World!\n");
+    char W1[1000] = "комар";
+    char W2[1000] = "корма";
+
+    int Tab1[256] = {0};
+    int Tab2[256] = {0};
+    if (strlen(W1) == strlen(W2))
+    {
+        while (*W1)
+        {
+            Tab1[*W1]++;
+        }
+        while (*W2)
+        {
+            Tab2[*W2]++;
+        }
+
+    }
+    else
+    {
+        printf("Words are not anagram!");
+    }
     return 0;
 }
