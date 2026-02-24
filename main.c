675c56d1bb2005da46d7ee8f0a69be4f6a86e50c
Nikolay Matskevich <kolyan_krytoj@mail.ru>
2026-02-24 11:52:35 +0700

    Added main logic for the task
    1) Added libraries stdbool.h and string.h
    2) Project devided in two parts: task and result

--- main.c
+++ main.c
@@ -1,28 +1,55 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
 int main()
 {
-    char W1[1000] = "комар";
-    char W2[1000] = "корма";
+    char W1[1000] = "dxdwd";
+    char W2[1000] = "xwddd";
+    char *ptrStr = W1;
+    bool flag = true;
 
     int Tab1[256] = {0};
     int Tab2[256] = {0};
+
     if (strlen(W1) == strlen(W2))
     {
-        while (*W1)
+        while (*ptrStr)
         {
-            Tab1[*W1]++;
-        }
-        while (*W2)
-        {
-            Tab2[*W2]++;
+            Tab1[*ptrStr]++;
+            ptrStr++;
         }
 
+        ptrStr = W2;
+        while (*ptrStr)
+        {
+            Tab2[*ptrStr]++;
+            ptrStr++;
+        }
+
+        for (int i = 0; i < 256; i++)
+        {
+            if (Tab1[i] != Tab2[i])
+            {
+                flag = false;
+            }
+        }
     }
+
     else
     {
-        printf("Words are not anagram!");
+        flag = false;
     }
+
+    if (flag)
+    {
+        printf("Words are anagram!\n");
+    }
+
+    else
+    {
+        printf("Words are not anagram!\n");
+    }
+
     return 0;
 }
