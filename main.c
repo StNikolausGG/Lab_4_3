ebcf1aa06c9980a6939a01b618db046caae91f91
Nikolay Matskevich <kolyan_krytoj@mail.ru>
2026-03-02 16:20:00 +0700

    Added function string_compress and new library
    1) Library: stdbool.h
    2) Added 3 reults of function: 0 - correct string compression, 1 - string 2 is empty, no compression, 2 - error (null address)
    3) Added test pointer for test null address

--- main.c
+++ main.c
@@ -1,7 +1,67 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+int string_compress(char *Word1, char *Word2)
+{
+    if (Word1 && Word2)
+    {
+        bool Tab[256] = {false};
+        bool flag = false;
+        char *ptrStr = Word2;
+
+        while(*ptrStr)
+        {
+            Tab[*ptrStr] = true;
+            flag = true;
+            ptrStr++;
+        }
+
+        if (!flag) return 1;
+
+        char *newStr = Word1;
+        ptrStr = Word1;
+
+        while (*ptrStr)
+        {
+            if (!Tab[*ptrStr])
+            {
+                *newStr = *ptrStr;
+                newStr++;
+            }
+            ptrStr++;
+        }
+        *newStr = '\0';
+
+        return 0;
+    }
+    return 2;
+}
+
 
 int main()
 {
-    printf("Hello World!\n");
+    char S1[1000] = "aabbcdhgkl";
+    char S2[1000] = "abd";
+    char *test = NULL;
+
+    printf("Uncompressed S1: %s\n", S1);
+    printf("S2: %s\n", S2);
+
+    int z = string_compress(S1, S2);
+
+    if (z == 0)
+    {
+        printf("Compressed S1: %s\n", S1);
+    }
+    else if (z == 1)
+    {
+        printf("S2 is empty, no compression. S1: %s\n", S1);
+    }
+    else
+    {
+        printf("One of arguments contains null address\n");
+    }
+
     return 0;
 }
