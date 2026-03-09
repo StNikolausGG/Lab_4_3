b5baa1f8bda1b82e33d3309f94bed721174e64da
Nikolay Matskevich <kolyan_krytoj@mail.ru>
2026-03-10 00:14:42 +0700

    Added more checks and tests, error localization, updated readme
    One of tests about cyr error, added check for null address, now function returns what`s right/wrong happened (0 - anagram, 1 - not anagram, 2 - null address error), added task for Readme.

--- main.c
+++ main.c
@@ -2,65 +2,98 @@
 #include <string.h>
 #include <stdbool.h>
 
-bool anagram_check(char *Word1, char *Word2)
+int anagram_check(char *Word1, char *Word2)
 {
-    char *ptrStr = Word1;
-    int Tab[256] = {0};
-
-    if (strlen(Word1) == strlen(Word2))
+    if (Word1 && Word2)
     {
-        while (*ptrStr)
+        if (strlen(Word1) == strlen(Word2))
         {
-            Tab[*ptrStr]++;
-            ptrStr++;
-        }
+            char *ptrStr = Word1;
+            int Tab[256] = {0};
 
-        ptrStr = Word2;
-        while (*ptrStr)
-        {
-            Tab[*ptrStr]--;
-            ptrStr++;
-        }
-
-        for (int i = 0; i < 256; i++)
-        {
-            if (Tab[i] != 0)
+            while (*ptrStr)
             {
-                return false;
+                Tab[*ptrStr]++;
+                ptrStr++;
             }
+
+            ptrStr = Word2;
+            while (*ptrStr)
+            {
+                Tab[*ptrStr]--;
+                ptrStr++;
+            }
+
+            for (int i = 0; i < 256; i++)
+            {
+                if (Tab[i] != 0)
+                {
+                    return 1;
+                }
+            }
+            return 0;
         }
-        return true;
+        else
+        {
+            return 1;
+        }
     }
-    return false;
+    return 2;
+
 }
 
 int main()
 {
+    //Базовая проверка подходящих слов
+//    char W1[1000] = "12345";
+//    char W2[1000] = "51423";
+    //Вывод: Words are anagram!
+
+    //Базовая проверка подходящих слов с повторяющимися символами
+//    char W1[1000] = "aabbcc";
+//    char W2[1000] = "abcabc";
+    //Вывод: Words are anagram!
+
     //Проверка условия длины
 //    char W1[1000] = "123";
 //    char W2[1000] = "3214";
     //Вывод: Words are not anagram!
 
-    //Проверка подходящих слов
-//    char W1[1000] = "dxdwd";
-//    char W2[1000] = "xwddd";
-    //Вывод: Words are anagram!
-
 //    //Проверка при W1 и W2 = "";
-    char W1[1000] = "";
-    char W2[1000] = "";
+//    char W1[1000] = "";
+//    char W2[1000] = "";
 //   Вывод: Words are anagram!
 
-    bool flag = anagram_check(W1, W2);
+    //Проверка нулевых адресов
+//    char *ptr1 = Null;
+//    char *ptr2 = Null;
+    //Вывод: Received null address!
 
-    if (flag)
+    //Проверка нулевого адреса
+//    char W1[1000] = "123";
+//    char *ptr2 = Null;
+    //Вывод: Received null address!
+
+    //Ошибка кириллицы
+    char W1[1000] = "о";
+    char W2[1000] = "я";
+    //Вывод: Words are anagram!
+
+    int flag = anagram_check(W1, W2);
+
+    if (flag == 0)
     {
         printf("Words are anagram!\n");
     }
 
-    else
+    else if (flag == 1)
     {
         printf("Words are not anagram!\n");
+    }
+
+    else if (flag == 2)
+    {
+        printf("Received null address!\n");
     }
 
     return 0;
